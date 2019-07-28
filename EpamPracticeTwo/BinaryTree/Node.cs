f14f183565da4c91044563789e9f17aa8047ca3f
AlexandrBelen <32227653+AlexandrBelen@users.noreply.github.com>
2019-07-28 18:13:49 +0300

    second practice

---
+++ EpamPracticeTwo/BinaryTree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    class Node<TNode>
+    {
+        public delegate int Comparator(TNode other);
+        private Comparator comparator;
+
+        public TNode Value { get; private set; }
+        public Node(TNode Value)
+        {
+
+        }
+    }
+}
